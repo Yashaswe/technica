e24ff07922051b75c9539c4a6df0058ec13aab37
Yashaswe <amatyayasu@gmail.com>
2023-10-21 16:44:46 -0400

    added inputEmoji

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import { Textarea } from "@chakra-ui/react";
 //theme.ts
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
+import InputEmoji from "./InputEmoji";
 
 const App = () => {
   return (
     <div id="main">
+      <InputEmoji />
       <Textarea placeholder="Here is a sample placeholder" />
     </div>
   );
---
+++ client/src/InputEmoji.jsx
@@ -0,0 +1,20 @@
+const InputEmoji = () => {
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <InputGroup size="md">
+      <Input
+        pr="4.5rem"
+        type={show ? "text" : "password"}
+        placeholder="Enter password"
+      />
+      <InputRightElement width="4.5rem">
+        <Button h="1.75rem" size="sm" onClick={handleClick}>
+          {show ? "Hide" : "Show"}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
+};
+export default InputEmoji;
