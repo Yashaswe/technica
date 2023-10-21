fb141ee18776ba8ed9aa4b0fde03be2610ad1c9d
Yashaswe <amatyayasu@gmail.com>
2023-10-21 17:01:03 -0400

    fixed error in inputEmoji for useState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { Textarea } from "@chakra-ui/react";
 
-//theme.ts
-import { extendTheme } from "@chakra-ui/react";
-import { mode } from "@chakra-ui/theme-tools";
 import InputEmoji from "./InputEmoji";
 
 const App = () => {
   return (
     <div id="main">
-      <InputEmoji />
+      <InputEmoji></InputEmoji>
       <Textarea placeholder="Here is a sample placeholder" />
     </div>
   );
--- client/src/InputEmoji.jsx
+++ client/src/InputEmoji.jsx
@@ -1,19 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+
 const InputEmoji = () => {
-  const [show, setShow] = React.useState(false);
+  const [show, setShow] = useState(true);
+
   const handleClick = () => setShow(!show);
 
   return (
-    <InputGroup size="md">
-      <Input
-        pr="4.5rem"
-        type={show ? "text" : "password"}
-        placeholder="Enter password"
-      />
-      <InputRightElement width="4.5rem">
-        <Button h="1.75rem" size="sm" onClick={handleClick}>
-          {show ? "Hide" : "Show"}
-        </Button>
-      </InputRightElement>
+    <InputGroup>
+      <Input placeholder="Enter Emoji" pr="4.5rem" />
+      {show ? (
+        <InputRightElement width="4.5rem" size="sm" onClick={handleClick}>
+          <Button h="1.75rem" size="sm">
+            Show
+          </Button>
+        </InputRightElement>
+      ) : (
+        <></>
+      )}
     </InputGroup>
   );
 };
