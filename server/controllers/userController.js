65eb1887e992068b1da4557b1b1214e3ad81b3c8
haphung00 <hangocdihoc@gmail.com>
2023-10-21 22:37:25 -0400

    establish backend

---
+++ server/controllers/userController.js
@@ -0,0 +1,98 @@
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
+import setCookie from '../helpers/setCookie.js'
+
+// @desc Get one user
+// @route GET /user/:username
+// @access Private
+const getUser = async (req, res) => {
+	const { username } = req.body
+	try {
+		const user = await User.findOne({ username })
+			.select('-password')
+			.lean()
+			.exec()
+		if (!user) {
+			return res.status(404).json({ error: 'User not found' })
+		}
+		return res.status(200).json({ user })
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+		console.log('Error in getUser: ', err.message)
+	}
+}
+
+// @desc Create new user
+// @route POST /user/signup
+// @access Public
+const signupUser = async (req, res) => {
+	try {
+		const { username, password } = req.body
+		const user = await User.findOne({ username }).select('-password').lean()
+
+		if (user) {
+			return res.status(400).json({ error: 'User already exists' })
+		}
+
+		if (password.length < 5) {
+			return res.status(400).json({ error: 'Password too short' })
+		}
+
+		const salt = Number(process.env.SALT)
+		const hashedPwd = await bcrypt.hash(password, salt)
+
+		const newUser = new User({
+			username,
+			password: hashedPwd,
+		})
+		await newUser.save()
+
+		if (!newUser) {
+			return res.status(500).json({ error: 'Unsuccessful Sign Up' })
+		}
+
+		setCookie(newUser._id, res)
+		delete newUser.password
+		return res.status(201).json({ newUser })
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+		console.log('Error in createNewUser: ', err.message)
+	}
+}
+
+// @desc Login user
+// @route POST /user/login
+// @access Public
+const loginUser = async (req, res) => {
+	try {
+		const { username, password } = req.body
+		const user = await User.findOne({ username }).lean()
+		const isPwdCorrect = await bcrypt.compare(password, user?.password || '')
+
+		if (!user || !isPwdCorrect) {
+			return res.status(400).json({ message: 'Invalid username or password' })
+		}
+
+		setCookie(user._id, res)
+		delete user.password
+		return res.status(200).json({ user })
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+		console.log('Error in loginUser: ', err.message)
+	}
+}
+
+// @desc Logout user
+// @route /POST /user/logout
+// @access Public
+const logoutUser = async (req, res) => {
+	try {
+		res.cookie('jwt', '', { maxAge: 1 })
+		return res.status(200).json({ message: 'User logged out' })
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+		console.log('Error in loginUser: ', err.message)
+	}
+}
+
+export { getUser, signupUser, loginUser, logoutUser }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
-import connectDB from './db/connectDB'
+import connectDB from './db/connectDB.js'
+import userRoutes from './routes/userRoutes.js'
+import storyRoutes from './routes/storyRoutes.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -12,4 +15,11 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
+
+// Routes
+app.use('/user', userRoutes)
+app.use('/story', storyRoutes)
+app.use('/', () => console.log('reached'))
+
 app.listen(port, () => console.log('listening on port ' + port))
