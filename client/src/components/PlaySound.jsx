5d1038878e42b5b3e464e6e225cd47c0daaffc2d
Yashaswe <amatyayasu@gmail.com>
2023-10-22 10:54:24 -0400

    added link to sound

--- client/src/components/PlaySound.jsx
+++ client/src/components/PlaySound.jsx
@@ -1,22 +1,25 @@
-import useSound from 'use-sound'
+import useSound from "use-sound";
 
-import { FaPlay } from 'react-icons/fa'
-import { useState } from 'react'
+import { FaPlay } from "react-icons/fa";
+import { useState } from "react";
 
-const PlaySound = (sound) => {
-	const [playing, setPlaying] = useState(false)
-	const [play] = useSound(sound)
+const PlaySound = (props) => {
+  const [playing, setPlaying] = useState(false);
+  const [play] = useSound(
+    "https://res.cloudinary.com/dinossasf/raw/upload/v1697984492/rfuffihawefgjqt4t1kc.mp3"
+  );
 
-	return (
-		<button
-			disabled={playing}
-			onClick={() => {
-				play()
-				setPlaying(true)
-			}}>
-			<FaPlay size={30}></FaPlay>
-		</button>
-	)
-}
+  return (
+    <button
+      disabled={playing}
+      onClick={() => {
+        play();
+        setPlaying(true);
+      }}
+    >
+      <FaPlay size={30}></FaPlay>
+    </button>
+  );
+};
 
-export default PlaySound
+export default PlaySound;
