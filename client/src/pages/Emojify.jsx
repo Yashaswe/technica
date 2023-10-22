10b63f2748225968c945d853ce313553afc78cd5
Yashaswe <amatyayasu@gmail.com>
2023-10-22 10:15:44 -0400

    add condition

--- client/src/pages/Emojify.jsx
+++ client/src/pages/Emojify.jsx
@@ -1,41 +1,42 @@
-import { Container, Flex } from '@chakra-ui/react'
-import InputEmoji from '../components/InputEmoji'
-import PlaySound from '../components/PlaySound'
-import TextPlace from '../components/TextPlace'
-import { useEffect, useState } from 'react'
+import { Container, Flex } from "@chakra-ui/react";
+import InputEmoji from "../components/InputEmoji";
+import PlaySound from "../components/PlaySound";
+import TextPlace from "../components/TextPlace";
+import { useEffect, useState } from "react";
 
 const Emojify = () => {
-	const [result, setResult] = useState({
-		response: '',
-		mp3: '',
-	})
-	const [showResult, setShowResult] = useState(false)
+  const [result, setResult] = useState({
+    response: "",
+    mp3: "",
+  });
 
-	useEffect(() => {
-		setShowResult(true)
-	}, [result])
+  const [showResult, setShowResult] = useState(false);
 
-	return (
-		<Flex flexDir={'column'}>
-			<InputEmoji result={result} setResult={setResult}/>
-			<Flex
-				flexDir={'column'}
-				paddingTop={10}
-				alignContent={'space-between'}
-				gap={5}>
-				{showResult && <Container>
-					<PlaySound />
-					<TextPlace
-						text="
-display: inline-flex does not make flex items display inline. It makes the flex container display inline. That is the only difference between display: inline-flex and display: flex. A similar comparison can be made between display: inline-block and display: block, and pretty much any other display type that has an inline counterpart.1
+  useEffect(() => {
+    if (result.response.length > 0) {
+      setShowResult(true);
+    }
+  }, [result]);
 
-There is absolutely no difference in the effect on flex items; flex layout is identical whether the flex container is block-level or inline-level. In particular, the flex items themselves always behave like block-level boxes (although they do have some properties of inline-blocks). You cannot display flex items inline; otherwise you don't actually have a flex layout.
-
-It is not clear what exactly you mean by vertically align"
-					/>
-				</Container>}
-			</Flex>
-		</Flex>
-	)
-}
-export default Emojify
+  return (
+    <Flex flexDir={"column"}>
+      <InputEmoji result={result} setResult={setResult} />
+      <Flex
+        flexDir={"column"}
+        paddingTop={10}
+        alignContent={"space-between"}
+        gap={5}
+      >
+        {showResult ? (
+          <Container>
+            <PlaySound />
+            <TextPlace text={result.response} />
+          </Container>
+        ) : (
+          <></>
+        )}
+      </Flex>
+    </Flex>
+  );
+};
+export default Emojify;
