4b297830c5ae0094bf9e2945c8a4e364dc1a1f1e
Yashaswe <amatyayasu@gmail.com>
2023-10-22 01:36:11 -0400

    clean up code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,79 @@
 import { Textarea } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Login";
 
 import InputEmoji from "./InputEmoji";
+import TextPlace from "./TextPlace";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route
-          exact
-          path="/login"
-          component={() => {
-            <div
-              style={{
-                width: "40%",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+    // <div
+    //   id="main"
+    //   style={{
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <div
+    //     style={{
+    //       width: "40%",
 
-                // borderRadius: 20,
-              }}
-            >
-              <Login />
-            </div>;
-          }}
-        />
-        <Route
-          exact
-          path="/input-emoji"
-          component={() => (
-            <div
-              id="main"
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <InputEmoji></InputEmoji>
-              <div className="text-input">
-                <Textarea placeholder="Here is a sample placeholder" />
-              </div>
-            </div>
-          )}
-        />
-      </Switch>
-    </Router>
+    //       borderRadius: 20,
+    //     }}
+    //   >
+    //     <Login />
+    //   </div>
+    // </div>
+
+    <div id="main">
+      <InputEmoji></InputEmoji>
+      <div className="text-input">
+        <TextPlace />
+      </div>
+    </div>
+
+    // <Router>
+    //   <Switch>
+    //     <Route
+    //       exact
+    //       path="/login"
+    //       component={() => {
+    //         <div
+    //           style={{
+    //             width: "40%",
+    //             display: "flex",
+    //             justifyContent: "center",
+    //             alignItems: "center",
+
+    //             // borderRadius: 20,
+    //           }}
+    //         >
+    //           <Login />
+    //         </div>;
+    //       }}
+    //     />
+    //     <Route
+    //       exact
+    //       path="/input-emoji"
+    //       component={() => (
+    //         <div
+    //           id="main"
+    //           style={{
+    //             display: "flex",
+    //             justifyContent: "center",
+    //             alignItems: "center",
+    //           }}
+    //         >
+    //           <InputEmoji></InputEmoji>
+    //           <div className="text-input">
+    //             <Textarea placeholder="Here is a sample placeholder" />
+    //           </div>
+    //         </div>
+    //       )}
+    //     />
+    //   </Switch>
+    // </Router>
   );
 };
 
